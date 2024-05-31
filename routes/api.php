bab2c83a38c78fd34f62c81dd57deab291606755
Wongnarin Sukwichai <wongnarin.s@msu.ac.th>
2024-05-31 11:48:52 +0700

    update

--- routes/api.php
+++ routes/api.php
@@ -28,6 +28,11 @@ use Illuminate\Support\Facades\Route;
 | be assigned to the "api" middleware group. Make something great!
 |
 */
+Route::resource('store', CheckinController::class);
+Route::resource('upload', UploadController::class);
+Route::post('checkout', [CheckinController::class, 'checkout']);
+Route::post('otin', [CheckinController::class, 'otin']);
+Route::post('otout', [CheckinController::class, 'otout']);
 
 Route::post('logout', [AuthController::class, 'logout']);
 
